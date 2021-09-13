6f981faa13a145c3351d0631d97009e882d8c1a3
Juan Cosentino <juanpecose@hotmail.com>
2021-09-13 10:47:43 -0300

    magos y elfos

---
+++ src/Library/Elfos.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+
+namespace Roleplay
+{
+    public class Elfos
+    {
+        private string nombre;
+        public int vida;
+        public ArrayList items;
+
+        public string Nombre
+        {
+            get ; set ;
+        }
+        public Elfos (string nombre)
+        {
+            this.Nombre = nombre;
+            this.vida = vida;
+            this.items = items;
+            
+        }
+
+        
+    }
+}
